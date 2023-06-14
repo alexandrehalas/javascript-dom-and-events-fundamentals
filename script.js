582ca5448a954a0a6884f3dde0868ee55a05785c
Alexandre Halas <alexandrehalas@gmail.com>
2023-06-14 18:50:15 -0300

    Implementing the gaming logic

--- script.js
+++ script.js
@@ -8,10 +8,35 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 */
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+console.log(secretNumber);
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
-    document.querySelector('.message').textContent = '❌ No number! ';
+    document.querySelector('.message').textContent = '😥 No number! ';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🥳 Correct Number! ';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⬆️ Too high! ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💀 Game Over ';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '⬇️ Too low! ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💀 Game Over ';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
