1cb50e7b9c33239b078b0798593898ce4afaeddf
Alexandre Halas <alexandrehalas@gmail.com>
2023-06-14 19:55:36 -0300

    Manipulating CSS styles

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '😥 No number! ';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct Number! ';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '⬆️ Too high! ';
